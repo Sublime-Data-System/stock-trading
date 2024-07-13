ab165fc63303c24bbb622c2d57217dbab5e9a509
nimit-sublime <nimit@sublimedatasys.com>
2024-07-13 19:08:26 +0530

    Add Stock Alert API

--- backend/src/routes/stock.route.ts
+++ backend/src/routes/stock.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { auth } from '../middlewares/auth'
-import { getStockList, getStockCandles } from '../controllers/stock.controller'
+import { getStockList, getStockCandles, setAlert } from '../controllers/stock.controller'
 
 const router: Router = Router();
 
 // TODO: Add auth middleware
 router.get('/', getStockList)
 router.post('/candle', getStockCandles)
+router.post('/alert', setAlert)
 
 export default router
