976d273aa7011939824a4315836869799fefd140
anant-sublime <anant@sublimedatasys.com>
2024-07-15 12:00:29 +0530

    feat: implemented email service

--- backend/src/service/email.service.ts
+++ backend/src/service/email.service.ts
@@ -1,6 +1,30 @@
+import { Transporter, createTransport } from 'nodemailer';
 
+const transporter: Transporter = createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+} as any);
 
-// TODO: Implement Email Service
 export const sendEmail = async (email: string, message: string): Promise<void> => {
   console.log(`Sending email to ${email} with message: ${message}`);
+
+  const mailOptions = {
+    from: process.env.FROM_EMAIL,
+    to: email,
+    subject: 'Stock Alert',
+    text: message,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+  
+    console.log('Email sent successfully');
+  } catch(error) {
+    console.log('Error sending email: ', error);
+  }
 };
